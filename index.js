9c668535c1f9edbb5a29a82a5562b957fe8d6c38
Mariya <mariaomware@gmail.com>
2024-03-21 23:51:24 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Angie", "Christian", "Thanos", "Alessa"];
+const eventType = "funeral";
+
+function writeCards(names, eventType) {
+   let messages = []
+   for (let i = 0; 1 < names.length; i++) {
+     let message  = `Thank you ${names[i]} for attending the ${eventType}.`;
+    messages.push(message);
+  }
+    return messages;
+}
+
+writeCards(["Angie", "Christian", "Thanos", "Alessa"], "funeral");
+
+function countDown(number) {
+   while (number >= 0) {
+     console.log(number--);
+   }
+ }
+  countDown(10);
